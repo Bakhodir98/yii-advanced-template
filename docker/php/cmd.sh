ca32a4b6207140f773748017f0bf784a40e0ec5c
Bakhodir98 <r.bakhodir98@gmail.com>
2022-02-11 17:16:33 +0500

    Added api directory and modified cmd.sh

--- docker/php/cmd.sh
+++ docker/php/cmd.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+sleep 10
 composer install
-sudo -H -u "$USER" bash -c 'php init --env=Development --overwrite=No'
-sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations'
-sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations'
-sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0'
-
+php init --env=Development --overwrite=No
+php yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations
+php yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations
+php yii migrate --interactive=0
+set -m
+php-fpm &
 fg %1
