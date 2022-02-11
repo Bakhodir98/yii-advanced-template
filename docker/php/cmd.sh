2131699f184a4765a14f2ff04bddb87f81da7715
Bakhodir98 <r.bakhodir98@gmail.com>
2022-02-11 15:44:52 +0500

    new env files, cmd, dockerfile, nginx changes

--- docker/php/cmd.sh
+++ docker/php/cmd.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-sleep 10
-#ping -c 1 host.docker.internal
-if [ $? -eq 0 ]; then
-  echo "xdebug.client_host=host.docker.internal" > $PHP_INI_DIR/conf.d/custom-002.ini
-else
-  echo "xdebug.client_host=$(ip route|awk '/default/ { print $3 }')" > $PHP_INI_DIR/conf.d/custom-002.ini
-fi
+
 composer install
-php init --env=Development --overwrite=No
-./yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations
-./yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations
-./yii migrate --interactive=0
-find backend/web/assets -maxdepth 1 ! -path backend/web/assets -type d -exec rm -rf {} \;
-find frontend/web/assets -maxdepth 1 ! -path frontend/web/assets -type d -exec rm -rf {} \;
-set -m
-php-fpm &
-#supercronic /tmp/my_crontab & ./yii chat/server-run
+sudo -H -u "$USER" bash -c 'php init --env=Development --overwrite=No'
+sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations'
+sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations'
+sudo -H -u "$USER" bash -c 'php yii migrate --interactive=0'
+
 fg %1
