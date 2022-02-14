352aeac5c4de6b1a27058e19accfb460003b778a
Bakhodir98 <r.bakhodir98@gmail.com>
2022-02-14 15:08:48 +0500

    Added yii-oauth2-server extension

--- docker/php/cmd.sh
+++ docker/php/cmd.sh
@@ -2,9 +2,10 @@
 sleep 10
 composer install
 php init --env=Development --overwrite=No
-php yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations
-php yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations
 php yii migrate --interactive=0
+php yii migrate --interactive=0 --migrationPath=@yii/rbac/migrations
+php yii migrate --interactive=0 --migrationPath=@vendor/filsh/yii2-oauth2-server/src/migrations
+
 set -m
 php-fpm &
 fg %1
