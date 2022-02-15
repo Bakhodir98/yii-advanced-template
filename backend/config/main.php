d6da1038e3e0b72dbc18d397e4b02a449588e74b
Mir-raid <mir6raid@gmail.com>
2022-02-15 12:32:13 +0500

    Giiant

--- backend/config/main.php
+++ backend/config/main.php
@@ -47,4 +47,12 @@ return [
         */
     ],
     'params' => $params,
+    'controllerMap' => [
+        'batch' => [
+            'class' => 'schmunk42\giiant\commands\BatchController',
+            'overwrite' => true,
+            'modelNamespace' => 'app\\modules\\crud\\models',
+            'crudTidyOutput' => true,
+        ]
+    ],
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,6 +36,18 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class'          => 'yii\i18n\PhpMessageSource',
+                    'basePath'       => '@app/messages', // if advanced application, set @frontend/messages
+                    'sourceLanguage' => 'en',
+                    'fileMap'        => [
+                        //'main' => 'main.php',
+                    ],
+                ],
+            ],
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
